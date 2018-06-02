9f19db97f7521e2a32c2c09f535838da88ada483
nyaboron1 <den.alihyder@protonmail.com>
2018-06-02 20:58:49 +0200

    kaprekar update

--- kaprekar/kaprekar.cpp
+++ kaprekar/kaprekar.cpp
@@ -1,8 +1,10 @@
-#include <string>
+#include <cmath>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-void completarCeros(std::vector<uint8_t>& digitos)
+void
+CompletarCeros (std::vector<uint8_t>& digitos)
 {
 	int16_t completar;
 
@@ -11,100 +13,65 @@ void completarCeros(std::vector<uint8_t>& digitos)
 	while (completar != 0)
 	{
 		digitos.push_back(0);
-		--completar;	
+		--completar;
 	}
 }
 
-void mergeDsc (std::vector<uint8_t>& vl, 
-		  	std::vector<uint8_t>& vr,
-		  	std::vector<uint8_t>& vs)
+
+void
+MergeSort (std::vector<uint8_t>& v)
 {
-
-	uint8_t s = 0;
-	uint8_t l = 0;
-	uint8_t r = 0;
-
-	while (l < vl.size() && r < vr.size())
-	{
-		if (vl[l] > vr[r])
-		vs[s++] = vl[l++];
-			
-		else
-			vs[s++] = vr[r++];
-	}
-	
-		while (l < vl.size())
-		vs[s++] = vl[l++];
-		
-		while (r < vr.size())
-		vs[s++] = vr[r++];
-		
-		completarCeros(vs);
-}
-
-void mergeAsc (std::vector<uint8_t>& vl, 
-		  	std::vector<uint8_t>& vr,
-		  	std::vector<uint8_t>& vs)
-{
-
-	uint8_t s = 0;
-	uint8_t l = 0;
-	uint8_t r = 0;
-
-	while (l < vl.size() && r < vr.size())
-	{
-		if (vl[l] < vr[r])
-		vs[s++] = vl[l++];
-			
-		else
-			vs[s++] = vr[r++];
-	}
-	
-		while (l < vl.size())
-		vs[s++] = vl[l++];
-		
-		while (r < vr.size())
-		vs[s++] = vr[r++];
-}
-
-void mergeSort (std::vector<uint8_t>& v, bool flag)
-{
-	uint16_t vsize = v.size();	
-	
-	if (vsize < 2)
+	if (v.size() < 2)
 		return;
 
 	else
 	{
+		uint16_t vsize = v.size();
 		uint16_t mid = v.size() / 2;
 		uint16_t ls = mid;
 		uint16_t rs = vsize - mid;
-	
-		std::vector<uint8_t> lv;
-		std::vector<uint8_t> rv;
+
+		std::vector<uint8_t> vl;
+		std::vector<uint8_t> vr;
 
 		for (uint8_t i = 0; i < ls; ++i)
-			lv.push_back(v[i]);
-		
+			vl.push_back(v[i]);
+
 		for (uint8_t i = mid; i < vsize; ++i)
-			rv.push_back(v[i]);
-		
-		mergeSort(lv,flag);
-		mergeSort(rv,flag);
-		
-		if (flag)
-			mergeAsc(lv, rv, v);
-		else
-			mergeDsc(lv, rv, v);
+			vr.push_back(v[i]);
 
+		MergeSort(vl);
+		MergeSort(vr);
 
+		uint8_t s = 0;
+		uint8_t l = 0;
+		uint8_t r = 0;
+
+		while (l < vl.size() && r < vr.size())
+		{
+			if (vl[l] > vr[r])
+				v[s++] = vl[l++];
+
+			else
+				v[s++] = vr[r++];
+		}
+
+		while (l < vl.size())
+			v[s++] = vl[l++];
+
+		while (r < vr.size())
+			v[s++] = vr[r++];
 	}
+
+	CompletarCeros(v);
 }
 
-std::vector<uint8_t> tovector(int16_t numero)
+
+std::vector<uint8_t>
+ToVector (int16_t numero)
 {
 	std::vector<uint8_t> digitos;
-	
+
 	while (numero > 0)
 	{
 		digitos.insert(digitos.begin(), numero%10);
@@ -115,9 +82,9 @@ std::vector<uint8_t> tovector(int16_t numero)
 }
 
 
-int16_t toint16_t(std::vector<uint8_t>& digitos)
+int16_t
+ToInt (std::vector<uint8_t>& digitos)
 {
-
 	int16_t factor = 1;
 	int16_t numero = 0;
 
@@ -130,121 +97,59 @@ int16_t toint16_t(std::vector<uint8_t>& digitos)
 	return numero;
 }
 
-
-bool repdigit(int16_t& numero)
+int16_t Kaprekar (int16_t& numero)
 {
+	int8_t vueltas = 0;
 
-	int16_t num = numero;
+	std::vector<uint8_t> v = ToVector(numero);
 
-	int16_t ultima = num % 10;
-	
-	bool es = true;
+	std::sort (v.begin(), v.end());
+	int16_t asc = ToInt(v);
 
-	num /= 10;
+	std::sort (v.begin(), v.end(), std::greater<uint8_t>());
+	int16_t desc = ToInt(v);
 
-	while (es && num != 0)
+	while ((desc - asc) != 6174)
 	{
-		if (ultima != num % 10)
-			es = false;
-		
-		num /= 10;
+		v = ToVector((desc - asc));
+
+		std::sort (v.begin(), v.end());
+		asc = ToInt(v);
+
+		MergeSort(v);
+		desc = ToInt(v);
+
+		++vueltas;
 	}
 
-	return es;
+	return vueltas+1;
 }
 
-
- int16_t kaprekar(int16_t& numero)
- {
- 		int16_t desc;
-		int16_t asc;
-		int16_t operar = 0;
-		
-		int8_t vueltas = 0;
-
-		std::vector<uint8_t> vnumero;
-		vnumero = tovector(numero);
-
-		mergeSort(vnumero, true);		
-		asc = toint16_t(vnumero);
-
-		mergeSort(vnumero, false);
-		desc = toint16_t(vnumero);		
-		
-		
-		while (operar != 6174)
-		{
-			operar = desc - asc;
-			
-			vnumero = tovector(operar);
-			
-			mergeSort(vnumero, true);
-			asc = toint16_t(vnumero);
-			
-			mergeSort(vnumero, false);
-			desc = toint16_t(vnumero);
-			
-			++vueltas;
-		}
-		
-		return vueltas;
- }
- 
-
-int16_t iteraciones(int16_t numero)
+void LeerFichero()
 {
-	if (numero == 6174)
-		return 0;
+	int16_t num;
+	int entradas;
+	std::cin >> entradas;
 
-	else if (repdigit(numero) && numero >= 1000)
-		return 8;
-
-	else
-		return kaprekar(numero);
-}
-
-
-void leerFichero()
-{
-	int16_t numero;
-
-	while(!std::cin.eof())
+	for (int i = 0; i < entradas; ++i)
 	{
-		std::cin >> numero;
-		
-		if (!std::cin.eof() && numero >= 1000)
-			std::cout << iteraciones(numero) << std::endl;
+		std::cin >> num;
+
+		if (num == 6174)
+			std::cout << 0 << std::endl;
+
+		else if ((num % ((int)pow (10, (int)(log10(num)) - 1) / 9)) == 0)
+			std::cout << 8 << std::endl;
+
+		else
+			std::cout << Kaprekar(num) << std::endl;
 	}
-	
+
 	std::cout << std::endl;
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-	leerFichero();
+	LeerFichero();
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
