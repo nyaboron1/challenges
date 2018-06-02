03c1cd1a40c8c72e8c7db7383e5afd68ca6171e5
nyaboron1 <den.alihyder@protonmail.com>
2018-06-02 17:59:26 +0200

    palindromo

--- palindromos/main.cpp
+++ palindromos/main.cpp
@@ -5,26 +5,22 @@
 int main ()
 {
   std::string line;
+  std::string inv;
   getline(std::cin, line);
 
   while (line.compare ("XXX") != 0)
   {
-    remove_if(line.begin(), line.end(), isspace);
+    //remove_if(line.begin(), line.end(), isspace);
+    line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
+    //line[0] = ::tolower(line[0]);
     std::transform(line.begin(), line.end(), line.begin(), ::tolower);
     std::string inv (line.rbegin(), line.rend());
 
-    std::cout << line << std::endl;
-    std::cout << inv << std::endl;
-    std::cout << std::endl;
+    if (line.compare(inv) == 0)
+      std::cout << "SI" << std::endl;
+    else
+      std::cout << "NO" << std::endl;
 
     getline(std::cin, line);
   }
 }
-/*
-
-
-
-
-
-
-*/
